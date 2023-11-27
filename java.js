2f37f86be0e9e36fb4f441b4096af538a777c629
SyangSouza <143663479+SyangSouza@users.noreply.github.com>
2023-11-27 19:28:57 -0300

    Delete java.js

--- java.js
+++
@@ -1,6 +0,0 @@
-const galleryButton = document.getElementById('gallery-button');
-const imageGallery = document.getElementById('image-gallery');
- 
-galleryButton.addEventListener('click', () => {
-    imageGallery.classList.toggle('hidden');
-});
